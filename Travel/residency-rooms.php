f301ebeb13edd141b360e7a3910305e0935812af
Swati Tiwari <swatitiwari354@gmail.com>
2022-11-26 09:36:18 +0530

    adding hotels and rooms

---
+++ Travel/residency-rooms.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wanderlust-Browse</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body class="browse-body">  
+    <div class="main">
+        <div class="opq-bg">
+            <div class ="topnav">
+                <div class="topdiva">
+                    <h3 class="tophead">Wanderlust</h3>
+                </div>
+                <div class="topdivb">
+                    <button class="topbuts">
+                        <a href="index.php">Home</a>
+                    </button>
+                    <?php 
+                       session_start();
+                       include ('conf.php');
+                        
+
+                        if(isset($_SESSION['is_logged_in'])){
+                            echo '<button class="topbuts">
+                                <a href="logout.php">Logout</a>
+                            </button>
+                            <button class="topbuts">
+                                <a href="booking.php">My Bookings</a>
+                            </button>';
+                        } else{
+                            echo "<button class='topbuts'>
+                                    <a href='login.php'>Login</a>
+                                </button>
+                                <button class='topbuts'>
+                                    <a href='signup.php'>Sign up</a>
+                                </button>";
+                        }
+                    ?>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="browse-page">
+            <div class="browse-l-wrapper">
+                <div class="browse-l">
+                    <div class="easy-ac-head cen">
+                        EASY ACCESS
+                    </div>
+                    <div class="easy-ac-body">
+                        <div class="tile">
+                            <div class="tile-img">
+                                <img src="./assets/tile-hotel.jpg" alt="tile-hotel" class="tile-icon">
+                            </div>
+                            <div class="tile-cont">
+                                <div class="tile-head"> 
+                                     Hotels
+                                </div>
+                                <div class="tile-desc">
+                                    Explore the most luxurious hotels at the best prices with us!
+                                </div>
+                                <div class="tile-link">
+                                    <button class="but-tile">
+                                        <a href="hotels.php" class="links">Explore</a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tile">
+                            <div class="tile-img">
+                                <img src="./assets/tile-cities.png" alt="tile-cities" class="tile-icon">
+                            </div>
+                            <div class="tile-cont">
+                                <div class="tile-head"> 
+                                     Cities
+                                </div>
+                                <div class="tile-desc">
+                                    Plan your trip to any city of your choice!
+                                </div>
+                                <div class="tile-link">
+                                    <button class="but-tile">
+                                        <a href="cities.php" class="links">Explore</a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tile">
+                            <div class="tile-img">
+                                <img src="./assets/tile-book.png" alt="tile-book" class="tile-icon">
+                            </div>
+                            <div class="tile-cont">
+                                <div class="tile-head"> 
+                                     Book hotel
+                                </div>
+                                <div class="tile-desc">
+                                    Make hotel bookings at affordable prices!
+                                </div>
+                                <div class="tile-link">
+                                    <button class="but-tile">
+                                        <a href="booking.php" class="links">Explore</a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tile">
+                            <div class="tile-img">
+                                <img src="./assets/tile-places.png" alt="tile-places" class="tile-icon">
+                            </div>
+                            <div class="tile-cont">
+                                <div class="tile-head"> 
+                                     Places
+                                </div>
+                                <div class="tile-desc">
+                                    Explore the most beautiful places to see!
+                                </div>
+                                <div class="tile-link">
+                                    <button class="but-tile">
+                                        <a href="places.php" class="links">Explore</a>
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        
+                        <?php
+                            if(isset($_SESSION['is_logged_in'])){
+                                echo '<div class="tile">
+                                    <div class="tile-img">
+                                        <img src="./assets/tile-booking.jpg" alt="tile-booking" class="tile-icon">
+                                    </div>
+                                    <div class="tile-cont">
+                                        <div class="tile-head"> 
+                                            My Bookings
+                                        </div>
+                                        <div class="tile-desc">
+                                            View your bookings and manage them!
+                                        </div>
+                                        <div class="tile-link">
+                                            <button class="but-tile">
+                                                <a href="mybookings.php" class="links">Explore</a>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>';
+                            }
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+            <div class="browse-r-wrapper">
+                <div class="browse-r">
+                    <div class="browse-r-body">
+                    <?php
+                        include('conf.php');
+                        $query1 = "SELECT * FROM rooms WHERE hotel_id = '105' and type_id = '12'";
+                        $query2 = "SELECT * FROM rooms WHERE hotel_id = '105' and type_id = '13'";
+                        $query3 = "SELECT * FROM rooms WHERE hotel_id = '105' and type_id = '14'";
+                        $res1 = mysqli_query($conn,$query1);
+                        $res2 = mysqli_query($conn,$query2);
+                        $res3 = mysqli_query($conn,$query3);
+
+                         $noor1=mysqli_fetch_assoc($res1);
+                         $noor2=mysqli_fetch_assoc($res2);
+                         $noor3=mysqli_fetch_assoc($res3);
+                    ?>
+                               
+                            <div class="top">
+                                <div class="tleft">
+                                   <h1><?php 
+                                   if(isset($noor1['room_type']))
+                                   echo $noor1['room_type'];
+                                   ?></h1>
+                                  <img src="assets\hotels\club-room.webp" alt="the-bastion img">
+                               </div>
+                               <div class="tright2">
+                              <ul>
+                                <li>
+                                Rent :  <?php 
+                                   if(isset($noor1['rent']))
+                                   echo $noor1['rent'] . ' /-';
+                                   ?>
+                                </li>
+                                <li>
+                                    <!-- 225 sq.ft -->
+                                    Number of rooms : <?php 
+                                   if(isset($noor1['no_of_rooms']))
+                                   echo $noor1['no_of_rooms'];
+                                   ?>
+                                </li>
+                                <li>
+                                    <!-- King Bed or 2 Twin Bed -->
+                                    Availability : <?php 
+                                   if(isset($noor1['availability']))
+                                   echo $noor1['availability'];
+                                   ?>
+                                </li>
+                              </ul>
+                               </div>
+
+                            </div>
+                            <div class="middle">
+                                <div class="mleft">
+                                    <h1><?php 
+                                   if(isset($noor2['room_type']))
+                                   echo $noor2['room_type'];
+                                   ?></h1>
+                                    <div class="mleft2">
+                                    <ul>
+                                        <li>
+                                        Rent :  <?php 
+                                   if(isset($noor2['rent']))
+                                   echo $noor2['rent'] . ' /-'; ?>
+                                        </li>
+                                        <li>
+                                            <!-- 400 sq.ft -->
+                                            Number of rooms : <?php 
+                                   if(isset($noor2['no_of_rooms']))
+                                   echo $noor2['no_of_rooms'];
+                                   ?>
+                                        </li>
+                                        <li>
+                                            <!-- King Bed -->
+                                            Availability : <?php 
+                                   if(isset($noor2['availability']))
+                                   echo $noor2['availability'];
+                                   ?>
+                                        </li>
+                                    </ul>
+                                    </div>
+                                    
+                                    
+                                   
+                                </div>
+                                <div class="mright2">
+                                    <img src="assets\hotels\day-use-room.jpg" alt="room-img">
+                                </div>
+
+                
+                            </div>
+                            <div class="bottom">
+                                <div class="bleft">
+                                    <h1><?php 
+                                   if(isset($noor3['room_type']))
+                                   echo $noor3['room_type'];
+                                   ?></h1>
+                                    <img src="assets\hotels\deluxe-private-room.webp" alt="reach">
+                                </div>
+                                <div class="bright2">
+                                    <ul>
+                                        <li>
+                                        Rent :  <?php 
+                                   if(isset($noor3['rent']))
+                                   echo $noor3['rent'] . ' /-'; ?>
+                                        </li>
+                                        <li>
+                                            <!-- 812 sq.ft -->
+                                            Number of rooms : <?php 
+                                   if(isset($noor3['no_of_rooms']))
+                                   echo $noor3['no_of_rooms'];
+                                   ?>
+                                        </li>
+                                        <li>
+                                            <!-- King Bed -->
+                                            Availability : <?php 
+                                   if(isset($noor3['availability']))
+                                   echo $noor3['availability'];
+                                   ?>
+                                        </li>
+                                    </ul>
+                                
+                                </div>
+                                
+                               
+                            </div>
+
+
+
+                    </div>
+                </div>
+                    
+            </div>
+        </div>
+        
+    </div>
+
+            
+
+   
+
+    
+</body>
+</html>
---
+++ Travel/the-bastion.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wanderlust-Browse</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body class="browse-body">  
+    <div class="main">
+        <div class="opq-bg">
+            <div class ="topnav">
+                <div class="topdiva">
+                    <h3 class="tophead">Wanderlust</h3>
+                </div>
+                <div class="topdivb">
+                    <button class="topbuts">
+                        <a href="index.php">Home</a>
+                    </button>
+                    <?php 
+                       session_start();
+                       include ('conf.php');
+                        
+
+                        if(isset($_SESSION['is_logged_in'])){
+                            echo '<button class="topbuts">
+                                <a href="logout.php">Logout</a>
+                            </button>
+                            <button class="topbuts">
+                                <a href="mybookings.php">My Bookings</a>
+                            </button>';
+                        } else{
+                            echo "<button class='topbuts'>
+                                    <a href='login.php'>Login</a>
+                                </button>
+                                <button class='topbuts'>
+                                    <a href='signup.php'>Sign up</a>
+                                </button>";
+                        }
+                    ?>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="browse-page">
+            <div class="browse-l-wrapper">
+                <div class="browse-l">
+                    <div class="easy-ac-head cen">
+                        EASY ACCESS
+                    </div>
+                    <div class="easy-ac-body">
+                        <div class="tile">
+                            <div class="tile-img">
+                                <img src="./assets/tile-hotel.jpg" alt="tile-hotel" class="tile-icon">
+                            </div>
+                            <div class="tile-cont">
+                                <div class="tile-head"> 
+                                     Hotels
+                                </div>
+                                <div class="tile-desc">
+                                    Explore the most luxurious hotels at the best prices with us!
+                                </div>
+                                <div class="tile-link">
+                                    <button class="but-tile">
+                                        <a href="hotels.php" class="links">Explore</a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tile">
+                            <div class="tile-img">
+                                <img src="./assets/tile-cities.png" alt="tile-cities" class="tile-icon">
+                            </div>
+                            <div class="tile-cont">
+                                <div class="tile-head"> 
+                                     Cities
+                                </div>
+                                <div class="tile-desc">
+                                    Plan your trip to any city of your choice!
+                                </div>
+                                <div class="tile-link">
+                                    <button class="but-tile">
+                                        <a href="cities.php" class="links">Explore</a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tile">
+                            <div class="tile-img">
+                                <img src="./assets/tile-book.png" alt="tile-book" class="tile-icon">
+                            </div>
+                            <div class="tile-cont">
+                                <div class="tile-head"> 
+                                     Book hotel
+                                </div>
+                                <div class="tile-desc">
+                                    Make hotel bookings at affordable prices!
+                                </div>
+                                <div class="tile-link">
+                                    <button class="but-tile">
+                                        <a href="booking.php" class="links">Explore</a>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tile">
+                            <div class="tile-img">
+                                <img src="./assets/tile-places.png" alt="tile-places" class="tile-icon">
+                            </div>
+                            <div class="tile-cont">
+                                <div class="tile-head"> 
+                                     Places
+                                </div>
+                                <div class="tile-desc">
+                                    Explore the most beautiful places to see!
+                                </div>
+                                <div class="tile-link">
+                                    <button class="but-tile">
+                                        <a href="places.php" class="links">Explore</a>
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        
+                        <?php
+                            if(isset($_SESSION['is_logged_in'])){
+                                echo '<div class="tile">
+                                    <div class="tile-img">
+                                        <img src="./assets/tile-booking.jpg" alt="tile-booking" class="tile-icon">
+                                    </div>
+                                    <div class="tile-cont">
+                                        <div class="tile-head"> 
+                                            My Bookings
+                                        </div>
+                                        <div class="tile-desc">
+                                            View your bookings and manage them!
+                                        </div>
+                                        <div class="tile-link">
+                                            <button class="but-tile">
+                                                <a href="mybookings.php" class="links">Explore</a>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>';
+                            }
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+            <div class="browse-r-wrapper">
+                <div class="browse-r">
+                    <div class="browse-r-body">
+                               
+                            <div class="top">
+                                <div class="tleft">
+                                   <h1>The Bastion</h1>
+                                  <img src="assets\hotels\the_bastion.webp" alt="the-bastion img">
+                               </div>
+                               <div class="tright">
+                               Close to Aura Mall, this modern property makes for
+                                a relaxing stay option with its stylish rooms, a dining 
+                                venue and easy access to popular areas of the city.
+                               </div>
+
+                            </div>
+                            <div class="middle">
+                                <div class="mleft">
+                                    <h1>The Rooms</h1>
+                                    <div class = "text">
+                                    Click below to check for the different rooms available!
+                                    <div class="but">
+                                        <button class="click">
+                                        <a href="bastion-rooms.php">Click here</a>
+                                        </button>
+                                    </div>
+                                    </div>
+                                    
+                                   
+                                </div>
+                                <div class="mright">
+                                    <img src="assets\hotels\executive.webp" alt="room-img">
+                                </div>
+
+                
+                            </div>
+                            <div class="bottom">
+                                <div class="bleft">
+                                    <h1>How to Reach</h1>
+                                    <img src="assets\destination\dest1.jpg" alt="reach">
+                                </div>
+                                <div class="bright">
+                                    <ul>
+                                        <li>
+                                        Raja Bhoj International Airport is 21.3 km away from the property.
+                                        </li>
+                                        <li>
+                                        Bhopal Junction Railway Station is 10.1 km away from the property.
+                                        </li>
+                                        <li>
+                                        Bhopal Bus Stand is 10.4 km away from the property.
+                                        </li>
+                                    </ul>
+                                
+                                </div>
+                                
+                               
+                            </div>
+
+
+
+                    </div>
+                </div>
+                    
+            </div>
+        </div>
+        
+    </div>
+
+            
+
+   
+
+    
+</body>
+</html>
